ad31289436088d8c322cb24a165c786c21e868d0
이덕준 <gochist@gmail.com>
2010-12-22 15:45:46 +0900

    대부분의 api 를 cover하도록 수정

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
-from townapi import TownApi, config, init_access_token, check_access_token
+from townapi import TownApi, init_access_token, check_access_token
 import unittest
 
 class TestTownApi(unittest.TestCase):
@@ -29,10 +29,64 @@ class TestTownApi(unittest.TestCase):
         q = "15"
         self.assertRaises(Exception, self.api.get_users_search, (q))
         
+    def test_get_boards_lookup(self):
+        board_id = "board_freeboard,board_alumni99,photo_alumni99"
+        boards = self.api.get_boards_lookup(board_id)
+        ids = [board['board_id'] for board in boards]
+        self.assertTrue('board_freeboard' in ids)
+        self.assertTrue('photo_alumni99' in ids)
+        
+    def test_get_boards_favorite(self):
+        boards = self.api.get_boards_favorite()
+        ids = [board['board_id'] for board in boards]
+        self.assertTrue('board_freeboard' in ids)
+        
+    def test_get_articles_list(self):
+        board_id = "board_alumni99"
+        response = self.api.get_articles_list(board_id)
+        articles = response['articles']
+        listinfo = response['listinfo']
+        self.assertEqual(listinfo['board_id'], board_id)
+        self.assertEqual(articles[0]['board_id'], board_id)
+        
+    def test_get_articles_show(self):
+        board_id = "board_alumni99"
+        article_id = 100
+        article = self.api.get_articles_show(board_id, article_id)
+        self.assertEqual(article['board_id'], board_id)
+        self.assertEqual(article['id'], article_id)
+        
+    def test_get_favorites(self):
+        favorites = self.api.get_favorites_list()
+        self.assertEqual(type(favorites), list)
+        
+    def test_post_articles_create(self):
+        board_id = "board_alumni99"
+        title = u"제목?"
+        message = u"메시지를 써볼까요....\n\n흐음... 줄바꾸기는?"
+        response = self.api.post_articles_create(board_id, title, message)
+        status = response['status']
+        article = response['article']
+        self.assertEqual(status, 'ok')
+        self.assertEqual(article['board_id'], board_id)
+        self.assertEqual(article['title'], title)
+        
+    def test_post_comments_create(self):
+        board_id = "board_alumni99"
+        article_id = 100
+        message = u'커멘트 테스트 하는 중....'
+        response = self.api.post_comments_create(board_id, article_id, message)
+        status = response['status']
+        comment = response['comment']
+        self.assertEqual(status, 'ok')
+        self.assertEqual(comment['board_id'], board_id)
+        self.assertEqual(comment['content'], message)
+        
 if __name__ == '__main__':
-    access_token = ('8f64c38e8c884a97b76dbd3384a57160',
-                    '77a9ce2fbf004386be753b39292f8272')
-    if not check_access_token(*access_token):
+    access_token = ('28767d0e02cf46d69eefb6e2a8b550f9',
+                    'a780c01643a1456bbebdb797b22ebcb3')
+    username = check_access_token(*access_token)
+    if not username:
         access_token = init_access_token()
-        print "access_token:", access_token
+        print "access_token =", access_token
     unittest.main()
--- src/townapi/__init__.py
+++ src/townapi/__init__.py
@@ -8,63 +8,6 @@ import json
 
 CONSUMER = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
 
-class TownApi(ClientBase):
-    def __init__(self, token_key=None, token_secret=None, cache=None,
-                 timeout=None, proxy_info=None):
-        token = None
-        if token_key:
-            token = oauth2.Token(token_key, token_secret)
-        ClientBase.__init__(self, CONSUMER, token, cache, timeout, proxy_info)
-    
-    def _parse_token_response(self, response):
-        resp, content = response
-        if resp['status'] != '200': 
-            raise Exception('%s HTTP Error' % resp['status'])
-        
-        result = dict(parse_qsl(content))
-        return result['oauth_token'], result['oauth_token_secret']
-    
-    def _parse_json_response(self, response):
-        resp, content = response
-        if resp['status'] != '200':
-            raise Exception('%s HTTP Error' % resp['status'])
-        obj = json.loads(content)
-        if type(obj) == dict:
-            if obj.get('status', 'ok') == 'error':
-                raise Exception(obj['message'])
-        return obj
-    
-    def _get(self, uri, param=None):
-        if param:
-            if not uri.endswith('?'):
-                uri += '?'
-            uri += urlencode(param)
-        response = self.request(uri, "GET")
-        return self._parse_json_response(response)
-    
-    def get_oauth_request_token(self, callback):
-        self.set_callback(callback)
-        response = self.request(REQUEST_TOKEN_URL, "GET")
-        self.callback = None
-        return self._parse_token_response(response)
-    
-    def post_oauth_access_token(self, verifier):
-        self.token.set_verifier(verifier)
-        response = self.request(ACCESS_TOKEN_URL, "POST")
-        return self._parse_token_response(response)
-    
-    def get_users_show(self, user_id=None):
-        uri = SERVICE_PROVIDER_URL + "/users/show/"
-        return self._get(uri)
-    
-    def get_users_lookup(self, user_id):
-        uri = SERVICE_PROVIDER_URL + "/users/lookup?"
-        return self._get(uri, {'user_id': user_id})
-
-    def get_users_search(self, q):
-        uri = SERVICE_PROVIDER_URL + "/users/search?"
-        return self._get(uri, {'q': q})
-
 def check_access_token(key, secret):
     "access_token이 유효한 경우 username을 반환한다."
     api = TownApi(key, secret)
@@ -92,7 +35,112 @@ def init_access_token():
         except:
             print "인증에 실패했습니다."
     
-    # get user
+    # check token
     username = check_access_token(key, secret)
     print username, "님 반갑습니다. 인증에 성공했습니다."
     return ret
+
+
+class TownApi(ClientBase):
+    def __init__(self, token_key=None, token_secret=None, cache=None,
+                 timeout=None, proxy_info=None):
+        token = None
+        if token_key:
+            token = oauth2.Token(token_key, token_secret)
+        ClientBase.__init__(self, CONSUMER, token, cache, timeout, proxy_info)
+    
+    def _parse_token_response(self, response):
+        resp, content = response
+        if resp['status'] != '200': 
+            raise Exception('%s HTTP Error' % resp['status'])
+        
+        result = dict(parse_qsl(content))
+        return result['oauth_token'], result['oauth_token_secret']
+    
+    def _parse_json_response(self, response):
+        resp, content = response
+        if resp['status'] != '200':
+            raise Exception('%s HTTP Error' % resp['status'])
+        obj = json.loads(content)
+        if type(obj) is dict:
+            if obj.get('status', 'ok') == 'error':
+                raise Exception(obj['message'])
+        return obj
+    
+    def _get(self, uri, param=None):
+        if not uri.startswith(SERVICE_PROVIDER_URL):
+            uri = SERVICE_PROVIDER_URL + uri
+        if param:
+            if not uri.endswith('?'):
+                uri += '?'
+            uri += urlencode(param)
+        response = self.request(uri, "GET")
+        return self._parse_json_response(response)
+
+    def _post(self, uri, params):
+        def to_utf8(buf):
+            if type(buf) is unicode:
+                return buf.encode('utf8')
+            return buf
+        
+        if not uri.startswith(SERVICE_PROVIDER_URL):
+            uri = SERVICE_PROVIDER_URL + uri
+            
+        if params:
+            params = dict([(k, to_utf8(v)) for k, v in params.iteritems()])
+        
+        body = urlencode(params)
+        response = self.request(uri, "POST", body)
+        
+        return self._parse_json_response(response)
+
+    def get_oauth_request_token(self, callback):
+        self.set_callback(callback)
+        response = self.request(REQUEST_TOKEN_URL, "GET")
+        self.callback = None
+        return self._parse_token_response(response)
+    
+    def post_oauth_access_token(self, verifier):
+        self.token.set_verifier(verifier)
+        response = self.request(ACCESS_TOKEN_URL, "POST")
+        return self._parse_token_response(response)
+    
+    def get_users_show(self, user_id=None):
+        if user_id:
+            return self._get("/users/show/%s" % user_id)
+        return self._get("/users/show")
+    
+    def get_users_lookup(self, user_id):
+        return self._get("/users/lookup?", {'user_id': user_id})
+
+    def get_users_search(self, q):
+        return self._get("/users/search?", {'q': q})
+    
+    def get_boards_favorite(self):
+        return self._get("/boards/favorite")
+    
+    def get_boards_lookup(self, board_id):
+        return self._get("/boards/lookup", {'board_id': board_id})
+    
+    def get_articles_list(self, board_id, page=0, per_page=20, q=""):
+        params = {'page':page, 'per_page':per_page, 'q':q}
+        return self._get("/articles/list/%s" % board_id, params)
+    
+    def get_articles_show(self, board_id, article_id):
+        return self._get("/articles/show/%s/%s" % (board_id, article_id))
+    
+    def get_favorites_list(self):
+        return self._get("/favorites/list")
+    
+    def post_articles_create(self, board_id, title, message):
+        uri = "/articles/create/%s" % board_id
+        params = {'title': title, 'message': message}
+        return self._post(uri, params)
+    
+    def post_comments_create(self, board_id, article_id, message):
+        uri = "/comments/create/%s/%s" % (board_id, article_id)
+        params = {'message': message}
+        return self._post(uri, params)
+
+        
+
