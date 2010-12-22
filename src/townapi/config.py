36b2cbed24175709310fecd4087f33e0a5eba524
이덕준 <gochist@gmail.com>
2010-12-22 09:19:57 +0900

    config 등록

--- src/townapi/config.py
+++ src/townapi/config.py
@@ -1,3 +1,9 @@
-from mprj.settings import CONSUMER_KEY, CONSUMER_SECRET, CALLBACK_URL
-from mprj.settings import SERVICE_PROVIDER_URL, REQUEST_TOKEN_URL
-from mprj.settings import ACCESS_TOKEN_URL, AUTHORIZE_URL
+CONSUMER_KEY = "consumer_key"
+CONSUMER_SECRET = "consumer_secret"
+
+CALLBACK_URL = "http://localhost:9000/accounts/callback"
+SERVICE_PROVIDER_URL = "http://localhost:8000"
+
+REQUEST_TOKEN_URL = SERVICE_PROVIDER_URL + "/oauth/request_token"
+ACCESS_TOKEN_URL = SERVICE_PROVIDER_URL + "/oauth/access_token"
+AUTHORIZE_URL = SERVICE_PROVIDER_URL + "/oauth/authorize"
